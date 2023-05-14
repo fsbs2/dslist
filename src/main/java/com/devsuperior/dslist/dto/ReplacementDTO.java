8e33bcc131f829e5880d482711d29edf6a9c378e
fsbs2 <gabriel_fsbs2@hotmail.com>
2023-05-14 15:37:00 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReplacementDTO {
+    private Integer sourceIndex;
+    private Integer targetIndex;
+}
